e2af0b4d4865b6b9d9dd40a9a701fc1496e88042
savioabuga <savioabuga@gmail.com>
2016-10-17 15:40:56 +0300

    send data to server via thunk

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import SignupForm from './SignupForm';
+import { userSignupRequest } from '../../actions/signupActions';
+import { connect } from 'react-redux';
 
-export default class SignupPage extends Component {
+class SignupPage extends Component {
 	render() {
+		const { userSignupRequest } = this.props;
 		return (
 			<div>
 				<h1>Join us</h1>
-				<SignupForm />
+				<SignupForm userSignupRequest={userSignupRequest} />
 			</div>
 		);
 	}
 }
+
+SignupPage.propTypes = {
+	userSignupRequest: React.PropTypes.func.isRequired
+}
+
+
+export default connect(null, {userSignupRequest})(SignupPage)
