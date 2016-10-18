4e1c66ce68a3ca83ed9e5ae10700c76de06e672e
savioabuga <savioabuga@gmail.com>
2016-10-18 08:22:51 +0300

    add flash message reducer

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import SignupForm from './SignupForm';
 import { userSignupRequest } from '../../actions/signupActions';
+import { addFlashMessage } from '../../actions/flashMessages';
 import { connect } from 'react-redux';
 
 class SignupPage extends Component {
 	render() {
-		const { userSignupRequest } = this.props;
+		const { userSignupRequest, addFlashMessage } = this.props;
 		return (
 			<div>
 				<h1>Join us</h1>
-				<SignupForm userSignupRequest={userSignupRequest} />
+				<SignupForm userSignupRequest={userSignupRequest} addFlashMessage={addFlashMessage} />
 			</div>
 		);
 	}
 }
 
 SignupPage.propTypes = {
-	userSignupRequest: React.PropTypes.func.isRequired
+	userSignupRequest: React.PropTypes.func.isRequired,
+	addFlashMessage: React.PropTypes.func.isRequired
 }
 
 
-export default connect(null, {userSignupRequest})(SignupPage)
+export default connect(null, {userSignupRequest, addFlashMessage})(SignupPage)
--- client/index.js
+++ client/index.js
@@ -4,12 +4,16 @@ import App from './components/App';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './rootReducer';
 
 const store = createStore(
-	(state = {}) => state,
-	applyMiddleware(thunk)
+	rootReducer,
+	compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	)
 );
 
 render(
